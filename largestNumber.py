82ee251158cf0450e6becb5bf1ac9978d34a3642
mohith789p <mohith321p@gmail.com>
2024-07-11 00:02:28 +0530

    Maximum and Minimum

---
+++ largestNumber.py
@@ -0,0 +1,7 @@
+a = int(input('Enter first number: '))
+b = int(input('Enter second number: '))
+c = int(input('Enter third number: '))
+print('Maximum Number is '+ str(max(a, b, c)))
+
+#similarly for min
+print('Minnimum Number is '+ str(min(a, b, c)))
