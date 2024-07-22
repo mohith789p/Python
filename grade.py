402a0e85fc8b23b811300980a9205be8dac19bed
mohith789p <mohith321p@gmail.com>
2024-07-22 16:28:28 +0530

    marks to grades

---
+++ grade.py
@@ -0,0 +1,13 @@
+marks = float(input('Enter your marks: '))
+if marks >= 90:
+    print('GRADE: A')
+elif marks >= 80:
+    print('GRADE: B')
+elif marks >= 70:
+    print('GRADE: C')
+elif marks >= 60:
+    print('GRADE: D')
+elif marks >= 50:
+    print('GRADE: E')
+else:
+    print('FAILED')
