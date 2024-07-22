9210c9303d5887c5a6ec4f493fb4d2af9e1fe87e
mohith789p <mohith321p@gmail.com>
2024-07-22 15:12:35 +0530

    simple interest

---
+++ simple_interest.py
@@ -0,0 +1,5 @@
+priniciple = float(input('Enter Priniciple Amount: '))
+time = float(input('Enter Time Taken: '))
+rate = float(input('Enter Rate of Interest: '))
+interest = (priniciple*time*rate)/100
+print('Simple Interest: ', interest)
