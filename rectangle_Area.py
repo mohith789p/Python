e19b43cd3c07954a62939ae3e86a023648ef3bf8
mohith789p <mohith321p@gmail.com>
2024-07-22 15:13:05 +0530

    Area and Perimeter of Rectangle

---
+++ rectangle_Area.py
@@ -0,0 +1,7 @@
+length = float(input('Enter the length: '))
+breadth = float(input('Enter the breadth: '))
+perimeter = 2*(length + breadth)
+area = length * breadth
+print("Perimeter: ",perimeter)
+print("Area: ",area)         
+                                                                      
