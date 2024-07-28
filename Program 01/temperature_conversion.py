3394f0730441158330a5f1537aed136cace07fdf
mohith789p <mohith321p@gmail.com>
2024-07-28 16:46:35 +0530

    temperature_conversion.py

---
+++ Program 01/temperature_conversion.py
@@ -0,0 +1,3 @@
+fahrenheit = float(input('Enter temperature in Fahreheit: '))
+celcius = (fahrenheit - 32) * 5 / 9 #converting fahrenheit to celcius
+print('Temperature in Celcius: ', celcius)
--- temperature_conversion.py
+++
@@ -1,3 +0,0 @@
-fahrenheit = float(input('Enter temperature in Fahreheit: '))
-celcius = (fahrenheit - 32) * 5 / 9
-print('Temperature in Celcius: ', celcius)
