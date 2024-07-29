f12bd681534fef988402a32f4140ff5c1bc041d1
mohith789p <mohith321p@gmail.com>
2024-07-29 15:17:22 +0530

    Add files via upload

---
+++ Program 03/table.py
@@ -0,0 +1,9 @@
+num = int(input('Enter a Number: '))
+# using for loop
+for i in range(1,11):
+    print(num,'x',i,'=',num*i)
+# using while
+i = 1
+while i<11:
+    print(num,'x',i,'=',num*i)
+    i+=1
