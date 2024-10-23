149b2402ab07cbd56b51464f4b0265edd039e478
mohith789p <mohith321p@gmail.com>
2024-10-23 23:35:06 +0530

    commited

---
+++ Program 07/2nd large.py
@@ -0,0 +1,10 @@
+def max(l):
+    l.sort()
+    print("the 2nd largest element in ",l,"is",l[-2])
+n = int(input('Enter the no. of elements: '))
+l = []
+print('Enter the content in the list: ')
+for i in range(n):
+    l.append(input())
+l=list(set(l))
+max(l)
---
+++ Program 07/maxMin.py
@@ -0,0 +1,8 @@
+def maxMin(l):
+    return max(l),min(l)
+n = int(input('Enter the no. of elements: '))
+l = []
+print('Enter the content in the list: ')
+for i in range(n):
+    l.append(input())
+print("largest and smallest in ",l,"is",maxMin(l))
